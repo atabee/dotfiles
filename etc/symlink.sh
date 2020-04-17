b330b292f1b9287395769f14e4bfecc72bf1ca81
Tanaka Geruge <tanakageruge@gmail.com>
2020-04-18 02:47:28 +0900

    Fix zsh config files

--- etc/symlink.sh
+++ etc/symlink.sh
@@ -2,22 +2,8 @@
 
 DOTPATH=${DOTPATH:-$HOME/.dotfiles}
 
-# XDG Base Directory
-export XDG_CONFIG_HOME=$HOME/.config
-export XDG_CACHE_HOME=$HOME/.cache
-export XDG_DATA_HOME=$HOME/.local/share
-
 # create symlink for dotfiles
 if [[ -f  $HOME/.zshrc ]]; then
   mv $HOME/.zshrc $HOME/.zshrc.$(date +'%Y%m%d%H%M%S').backup
 fi 
 ln -s $DOTPATH/zsh/.zshrc $HOME/.zshrc
-
-# directories
-link_dirs=(zsh)
-for dir in "${link_dirs[@]}"; do
-  if [ -d "$XDG_CONFIG_HOME/$dir" ]; then
-    mv $XDG_CONFIG_HOME/$dir $XDG_CONFIG_HOME/$dir.$(date +'%Y%m%d%H%M%S').backup
-  fi
-  ln -snf $DOTPATH/$dir $XDG_CONFIG_HOME/$dir
-done
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,15 +1,9 @@
 #! /usr/bin/env zsh
 
-# XDG Base Directory
-export XDG_CONFIG_HOME=$HOME/.config
-export XDG_CACHE_HOME=$HOME/.cache
-export XDG_DATA_HOME=$HOME/.local/share
+DOTPATH=${DOTPATH:-$HOME/.dotfiles}
 
 # zsh home directory
-export ZDOTDIR=$XDG_CONFIG_HOME/zsh
-
-# history
-export HISTFILE=$XDG_DATA_HOME/zsh/history
+export ZDOTDIR=$DOTPATH/zsh
 
 if [[ -s "${ZDOTDIR}/init.zsh" ]]; then
   source "${ZDOTDIR}/init.zsh"
