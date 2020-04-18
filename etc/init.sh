669914a484a4b6bb6f083042f6c42fa2ea5a8183
Tanaka Geruge <tanakageruge@gmail.com>
2020-04-18 22:02:35 +0900

    Fix bug of install script

--- etc/init.sh
+++ etc/init.sh
@@ -2,20 +2,10 @@
 
 # Path settings
 DOTPATH=${DOTPATH:-$HOME/.dotfiles}
-REPO=TanakaGeruge/dotfiles
 
 # load environment
 source $DOTPATH/etc/env.sh
 
 # install tools
-println "Install git..."
-install_git
-
 println "Install fzf..."
 install_fzf
-
-# Download dotfiles
-if [ ! -d "$DOTPATH" ]; then
-  println "Downloading dotfiles..."
-  git clone --recursive "https://github.com/$REPO" "$DOTPATH"
-fi
