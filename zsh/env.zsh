d8aff45b82c76ebffba34768f29b6f3a5c73943b
Tanaka Geruge <tanakageruge@gmail.com>
2020-12-08 21:47:58 +0900

    Update env.zsh
    Add nvm and ruby configuration

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -23,12 +23,19 @@ fi
 #export IPYTHONDIR="$XDG_CONFIG_HOME/jupyter"
 #export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME/jupyter"
 
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+#[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
 # npm
 #export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
 
 # Ruby Gem
 #export GEM_HOME="$XDG_DATA_HOME/gem"
 #export GEM_SPEC_CACHE="$XDG_CACHE_HOME/gem"
+export PATH="/usr/local/opt/ruby/bin:$PATH"
+export PATH="$HOME/.gem/ruby/2.7.0/bin:$PATH"
 
 # openssl
 #export PATH=/usr/local/opt/openssl@1.1/bin:$PATH
