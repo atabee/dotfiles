b67b8c0ead833de1226a56f9e0fbc0df4589c2f2
tailorsnine <tailorsnine@gmail.com>
2023-09-25 10:22:48 +0900

    Add java path to use Android Studio JDK

--- zsh/os/Darwin.zsh
+++ zsh/os/Darwin.zsh
@@ -1,27 +1,13 @@
-if (( $+commands[brew] )); then
-  # brew docterでpyenvのpathがエラーが出るので
-  alias brew="env PATH=${PATH/\/Users\/${USER}\/\.pyenv\/shims:/} brew"
-
-  function update() {
-    brew update
-    brew upgrade
-    brew cask upgrade
-    brew cleanup
-  }
-fi
-
-# java
-#if (( $+commands[java] )); then
-#  export JAVA_HOME=$(/usr/libexec/java_home -v 11)
-#  export PATH="$JAVA_HOME/bin:$PATH"
-#fi
-
 # Android
 export PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"
 export PATH="$HOME/Library/Android/sdk/tools:$PATH"
 if (( $+commands[android] )); then
   export ANDROID_HOME="$HOME/Library/Android/sdk"
 fi
+
+# java
+export JAVA_HOME=/Applications/"Android Studio.app"/Contents/jbr/Contents/Home
+export PATH="$JAVA_HOME/bin:$PATH"
 
 # dart
 export PATH="$HOME/.pub-cache/bin:$PATH"
