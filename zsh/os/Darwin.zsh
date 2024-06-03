46d42e145bd22199a60b3ba8f7073fba07cdcb77
atabichi <atabicher@gmail.com>
2024-06-03 20:49:03 +0900

    Add NDK_HOME

--- zsh/os/Darwin.zsh
+++ zsh/os/Darwin.zsh
@@ -3,6 +3,7 @@ export PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"
 export PATH="$HOME/Library/Android/sdk/tools:$PATH"
 if (( $+commands[android] )); then
   export ANDROID_HOME="$HOME/Library/Android/sdk"
+  export NDK_HOME=$ANDROID_HOME/ndk/26.1.10909125
 fi
 
 # dart
